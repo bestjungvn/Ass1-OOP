799c3543c2fe254b1c1549a3be372bf10c6c0f07
huylenguyen806 <nlhuy1998@gmail.com>
2017-10-28 23:53:43 +0700

    Punch and Kick

---
+++ Stickman/Assets/Tutorial/Scripts/PlayerInput.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerInput : MonoBehaviour {
+
+    private PlayerMovement c_movement;  //Reference to PlayerMovement script
+    private bool isJumping;             //To determine if the player is jumping
+	Animator anim;
+	int punchHash = Animator.StringToHash("punch");
+	int kickHash = Animator.StringToHash("kick");
+	
+	void Awake()
+    {
+        //References
+        c_movement = GetComponent<PlayerMovement>();
+		anim = GetComponent<Animator> ();
+	}
+	
+	void Update ()
+    {
+        //If he is not jumping...
+		if (!isJumping) {
+			//See if button is pressed...
+			isJumping = CrossPlatformInputManager.GetButtonDown ("Vertical");
+		}
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			anim.SetTrigger (punchHash);
+		}
+		if (Input.GetKeyDown(KeyCode.W)) {
+			anim.SetTrigger (kickHash);
+		}
+	}
+
+    private void FixedUpdate()
+    {
+        //Get horizontal axis
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        //Call movement function in PlayerMovement
+        c_movement.Move(h, isJumping);
+        //Reset
+        isJumping = false;
+    }
+}
